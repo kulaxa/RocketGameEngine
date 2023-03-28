66b91d1b047944fd9fb5123f3eccbcf5ab5fdc98
Mario Jalšovec <mario.jalsovec@mindsmiths.com>
2023-03-28 17:22:14 +0200

    cmake setup

---
+++ CMakeFiles/rocket.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rocket.dir/main.cpp.o"
+  "CMakeFiles/rocket.dir/main.cpp.o.d"
+  "rocket"
+  "rocket.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rocket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
